780884eb5f36624b08495a99ef4d533847c7ed6f
abdelrahmannabil987 <abdelrahmannabil987@gmail.com>
2025-01-22 12:59:15 +0200

    ADC Driver

---
+++ 5-ADC/ADC_private.h
@@ -0,0 +1,59 @@
+/*****************************************/
+/*****************************************/
+/***************  MHAL LAYER  ************/
+/***************  A.NABIL    *************/
+/***************  ADC_PRIVATE_H_ ********/
+/*****************************************/
+/*****************************************/
+
+
+#ifndef ADC_PRIVATE_H_
+#define ADC_PRIVATE_H_
+
+
+#define External 1
+#define AVCC 2
+#define Internal 3
+
+#define Left_Adj 1
+#define Right_Adj 2
+
+#define IDLE       1
+#define BUSY       2
+
+#define Division_By_Two                  1
+#define Division_By_Four                 2
+#define Division_By_Eight                3
+#define Division_By_Six_Teen             4
+#define Division_By_Thirty_Two           5
+#define Division_By_Sixty_Four           6
+#define Division_By_One_Twenty_Eight     7
+
+#define enabled   1
+#define disabled  2
+
+#define SingleChannelAsynch  0
+#define ChainAsynch          1
+
+
+#define PreScalar_Masking 0b11111000
+#define InputChannel_Masking 0b1100000
+
+
+
+
+#define Free_Running                        1
+#define Analog_Comparator                   2
+#define External_Interrupt_Request0         3
+#define Timer_Counter0_Compare_Match        4
+#define Timer_Counter0_Overflow             5
+#define Timer_Counter_Compare_Match_B       6
+#define Timer_Counter1_Overflow             7
+#define Timer_Counter1_Capture_Event        8
+
+
+
+
+
+
+#endif
---
+++ 5-ADC/ADC_register.h
@@ -0,0 +1,51 @@
+/*****************************************/
+/*****************************************/
+/***************  MHAL LAYER  ************/
+/***************  A.NABIL    *************/
+/***************  ADC_REGISTER_H_ ********/
+/*****************************************/
+/*****************************************/
+
+
+#ifndef ADC_REGISTER_H_
+#define ADC_REGISTER_H_
+
+
+#define ADMUX      *((volatile u8*)0x27) //ADC MULTIPLICTION SELECTION REGISTER
+                                         
+#define ADMUX_REFS1          7           //Reference Selection Bit1
+#define ADMUX_REFS0          6           //Reference Selection Bit0
+#define ADMUX_ADLAR          5           //ADC left adjust result
+
+#define ADCSRA      *((volatile u8*)0x26)  //ADC control status register A
+
+#define ADCSRA_ADEN       7      //ADC Enable
+#define ADCSRA_ADSC       6      //Start Conversion (take a screen shot of the current analog value (one time))
+#define ADCSRA_ADATE      5      //ADC AUTO trigger Enable (another src triggers the ADC like interrupt)
+#define ADCSRA_ADIF       4      //Interrupt Flag
+#define ADCSRA_ADIE       3      //Interrupt Enable
+#define ADCSRA_ADPS2      2      //Prescalar Bit2
+#define ADCSRA_ADPS1      1      //Prescalar Bit1
+#define ADCSRA_ADPS0      0      //Prescalar Bit0
+
+
+#define ADCH     *((volatile u8*)0x25)     //ADC High Register
+#define ADCL      *((volatile u8*)0x24)    //ADC Low Register
+
+#define SFIOR     *((volatile u8*)0x50)    //Special Function Register to choose the Auto Trigger Source
+#define SFIOR_ADTS0   5
+#define SFIOR_ADTS1   6
+#define SFIOR_ADTS2   7
+
+
+#define ADC_DATA *((volatile u16*)0x24)
+
+
+
+
+
+
+
+
+
+#endif
